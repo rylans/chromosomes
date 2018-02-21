5ad007d97b867fd032cd7ac8f0742cc4f92ba3be
Rylan <rylans@gmail.com>
2018-02-21 14:21:57 -0800

    Refactor optimize tests

--- optimize/optimize_test.go
+++ optimize/optimize_test.go
@@ -1,6 +1,7 @@
 package optimize 
 
 import (
+  "math/bits"
   "testing"
   "github.com/stretchr/testify/assert"
   "github.com/rylans/chromosomes"
@@ -14,15 +15,33 @@ func mostOnesFitness(c *chromosomes.Chromosome) float64 {
   return float64(c.Get(GENE1)) + float64(c.Get(GENE2)) + float64(c.Get(GENE3))
 }
 
+func sumLeadingZeros(c *chromosomes.Chromosome) float64 {
+  z1 := bits.LeadingZeros8(c.Get(GENE1))
+  z2 := bits.LeadingZeros8(c.Get(GENE2))
+  z3 := bits.LeadingZeros8(c.Get(GENE3))
+  return float64(z1) + float64(z2) + float64(z3)
+}
+
 func TestOptimizeImprovesChromosomeFromRandom(t *testing.T){
+  assertOptimizeImproves(t, mostOnesFitness, threeGeneBuilder())
+}
+
+func TestOptimizeImprovesChromosomeFromRandomLeadingZeros(t *testing.T){
+  assertOptimizeImproves(t, sumLeadingZeros, threeGeneBuilder())
+}
+
+func assertOptimizeImproves(t *testing.T, f func(c *chromosomes.Chromosome) float64, b *chromosomes.ChromosomeBuilder) {
+  random := b.BuildRandom()
+  result := Optimize(f, b)
+
+  assert.Equal(t, true, f(result) > f(random))
+}
+
+func threeGeneBuilder() *chromosomes.ChromosomeBuilder {
   b := chromosomes.NewBuilder()
   b.MutationChance(0.15)
   b.AddTrait(GENE1)
   b.AddTrait(GENE2)
   b.AddTrait(GENE3)
-
-  random := b.BuildRandom()
-  result := Optimize(mostOnesFitness, b)
-
-  assert.Equal(t, true, mostOnesFitness(result) > mostOnesFitness(random))
+  return b
 }
